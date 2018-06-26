d4fd7e756cd8ca3437de45ea47fea6cb9fe967e7
thoum <test>
2018-06-26 12:21:12 +0000

    render walls based on fov

--- rendering.py
+++ rendering.py
@@ -2,19 +2,19 @@ import curses
 import colors
 
 def render_all(win, entities, game_map, width, height, player_x, player_y,
-               stdscr):
-    draw_map(win, game_map)
+               fov_recompute):
+    draw_map(win, game_map, fov_recompute)
     for e in entities:
         draw_entity(win, e)
 
     # map_height, map_width = game_map.height, game_map.width
     start_x = min(max(0, player_x - width // 2), game_map.width - width)
     start_y = min(max(0, player_y - height // 2), game_map.height - height)
-    stdscr.addstr(20, 100, "x:" + str(player_x), colors.COLOR_WHITE_BOLD)
-    stdscr.addstr(21, 100, "y:"+str(player_y), colors.COLOR_WHITE_BOLD)
+    # stdscr.addstr(20, 100, "x:" + str(player_x), colors.COLOR_WHITE_BOLD)
+    # stdscr.addstr(21, 100, "y:"+str(player_y), colors.COLOR_WHITE_BOLD)
     # stdscr.addstr(22, 100, "w:" + str(start_x), colors.COLOR_WHITE_BOLD)
     # stdscr.addstr(23, 100, "h:"+str(start_y), colors.COLOR_WHITE_BOLD)
-    stdscr.refresh()
+    # stdscr.refresh()
     win.refresh(start_y, start_x, 0, 0, height, width)
 
 def clear_all(win, entities):
@@ -27,11 +27,16 @@ def draw_entity(win, entity):
 def clear_entity(win, entity):
     win.addch(entity.y, entity.x, ' ', curses.color_pair(1))
 
-def draw_map(win, game_map):
+def draw_map(win, game_map, fov_recompute):
     for x, y in game_map:
         wall = not game_map.transparent[x, y]
-
-        if wall:
-            win.addch(y, x, '#', colors.COLOR_DARK_GRAY)
+        if game_map.fov[x, y]:
+            if wall:
+                win.addch(y, x, '#', colors.COLOR_LIGHT_GRAY)
+            else:
+                win.addch(y, x, '.', colors.COLOR_GRAY)
         else:
-            win.addch(y, x, '^', colors.COLOR_GREEN)
+            if wall:
+                win.addch(y, x, '#', colors.COLOR_BLACK)
+            else:
+                win.addch(y, x, '.', colors.COLOR_BLACK)
