bbeb57bb01f34d3c2c95f36c9893066411b6f43f
thoum <test>
2018-07-02 10:24:15 +0000

    add basic components for entities

---
+++ components/ai.py
@@ -0,0 +1,4 @@
+class BasicMonster():
+    """Basic AI for mobs"""
+    def take_turn(self):
+        print(self.name)
---
+++ components/combat.py
@@ -0,0 +1,6 @@
+class Combat():
+    """the combat module for all entities that can combat"""
+    def __init__(self, hp, defense, power):
+        self.hp = hp
+        self.defense = defense
+        self.power = power
