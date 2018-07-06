2195110cc0d07cb68c16e0fc3db384cddacc5eec
thoum <test>
2018-07-06 09:54:46 +0000

    add diagonal movement

--- input_handler.py
+++ input_handler.py
@@ -1,14 +1,25 @@
+import curses
+
+
 def handle_input(win):
     user_input = win.getkey()
     # Movement keys
-    if user_input == 'k':
+    if user_input == 'k' or user_input == 'KEY_UP':
         return {'move': (0, -1)}
-    elif user_input == 'j':
+    elif user_input == 'j' or user_input == 'KEY_DOWN':
         return {'move': (0, 1)}
-    elif user_input == 'h':
+    elif user_input == 'h' or user_input == 'KEY_LEFT':
         return {'move': (-1, 0)}
-    elif user_input == 'l':
+    elif user_input == 'l' or user_input == 'KEY_RIGHT':
         return {'move': (1, 0)}
+    elif user_input == 'y':
+        return {'move': (-1, -1)}
+    elif user_input == 'u':
+        return {'move': (1, -1)}
+    elif user_input == 'b':
+        return {'move': (-1, 1)}
+    elif user_input == 'n':
+        return {'move': (1, 1)}
     else:
         return {'exit': True}
 
