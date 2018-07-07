8c2520ead2fc7be11b9f31d407d21dd0e2956a89
thoum <test>
2018-07-07 08:09:49 +0000

    add max_hp

--- components/combat.py
+++ components/combat.py
@@ -1,6 +1,7 @@
 class Combat():
     """the combat module for all entities that can combat"""
     def __init__(self, hp, defense, power):
+        self.max_hp = hp
         self.hp = hp
         self.defense = defense
         self.power = power
